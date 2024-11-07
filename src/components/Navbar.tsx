85ea5f05c5d437137651fd5d0cecc2ee6d459295
DominikTenerife <dpiskorski1@gmail.com>
2024-11-07 19:38:02 +0000

    navbar from antd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,40 @@
 // src/components/Navbar.tsx
 import Link from 'next/link';
+import { Menu } from 'antd';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem(<Link href="/">Home</Link>, 'home', <MailOutlined />),
+  getItem(<Link href="/upload">Upload</Link>, 'upload', <AppstoreOutlined />),
+  getItem(<Link href="/counter">Counter</Link>, 'counter', <SettingOutlined />),
+];
 
 export default function Navbar() {
   return (
-    <nav>
-    
-        <Link href="/">Home</Link>
-     
-        <Link href="/upload">Upload</Link>
-     
-        <Link href="/counter">Counter</Link>
-    </nav>
+    <Menu
+      mode="vertical"
+      theme="dark"
+      style={{ height: '100vh', width: 256 }}
+      items={items}
+    />
   );
-     
 }
