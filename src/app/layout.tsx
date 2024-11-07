c68ad052cff522dd0eb280c0982a42834af308d7
DominikTenerife <dpiskorski1@gmail.com>
2024-11-07 15:34:02 +0000

    moving globals.css to /styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../styles/globals.css";
 import ClientProvider from './ClientProvider';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
