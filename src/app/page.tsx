d1605eafe312865d0f8da90c668a149be9c259b6
DominikTenerife <dpiskorski1@gmail.com>
2024-11-07 09:22:04 +0000

    adding front image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 // src/app/page.tsx
 "use client";
 
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is El Instituto Home Page</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">This is El Instituto Home Page</h1>
+        <Image
+          src="/images/teenagers.png"
+          alt="Teenagers"
+          width={500}
+          height={300}
+          className="rounded-md"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import teenagersImg from '../images/teenagers.jpg';
+
 export default function Header() {
     return (
       <header className="bg-gray-800 p-4 text-white text-center">
         <h1>El Instituto</h1>
+        
       </header>
     );
   }
